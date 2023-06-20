b55b47b1943ada62f5b98434b539a93af58499dd
DaniSoftville <danisoftville@gmail.com>
2023-06-20 17:27:44 +0100

    Installation and Setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <>
+      <div className="bg-gray-20">Application</div>
+      <div className="bg-gray-50">Application</div>
+      <div className="bg-gray-500">Application</div>
+      <div className="bg-primary-100">Application</div>
+      <div className="bg-primary-300">Application</div>
+      <div className="bg-primary-500">Application</div>
+      <div className="bg-secondary-400">Application</div>
+      <div className="bg-secondary-500">Application</div>
+      <div className="bg-secondary-500">Application</div>
+    </>
+  );
+}
+
+export default App;
